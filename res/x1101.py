3e1568c006fdb04be5c5893cf652c9494b63623a
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-21 13:04:22 +0700

    Update x1101.py

--- res/x1101.py
+++ res/x1101.py
@@ -22,8 +22,12 @@ git_path = os.path.join(ui_path, "extensions")
 def kontolondon(oppai, asu, si_kontol, kntl):   
     start_time = time.time() 
     cprint(f"    > {asu}", color="flat_cyan")
-    try:
-        #subprocess.run(oppai, check=True, shell=True, text=True)
+    if debug:
+        try:
+        subprocess.run(oppai, check=True, shell=True, text=True)
+        si_kontol += 1   
+    else:
+        try
         subprocess.run(oppai, check=True, shell=True, text=True,  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         si_kontol += 1
     except subprocess.CalledProcessError as e:
