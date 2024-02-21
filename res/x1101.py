8d6fae292c97cc191f5fa318285e34cf54bc530a
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-02-21 12:19:04 +0700

    Update x1101.py

--- res/x1101.py
+++ res/x1101.py
@@ -54,18 +54,17 @@ if __name__ == "__main__":
         env = 'Sagemaker Studio Lab'
         agus.append(("conda install -q -y aria2", "aria2"))
         agus.append(("conda install -q -y glib"))
+        agus.append(("pip install -q opencv-python-headless huggingface-hub", "opencv-python-headless huggingface-hub"))
+        agus.append(("pip install -q --upgrade torchsde", "torchsde"))
+        agus.append(("pip install -q psutil", "psutil"))
+        agus.append(("pip install -q pytz"))
+        
         agus.append(("conda update -q -n base conda", "Updating Conda..."))
-        
-        agus.append(("pip install -q opencv-python-headless huggingface-hub"))
-        agus.append((""))
-        
-        
+        agus.append(("pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118", "Installing torch..."))
+        agus.append(("pip install -q xformers==0.0.20 triton==2.0.0", "Installing xformers..."))    
     else:
         cprint("Error. Enviroment not detected !")
-        
-
-!pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 -U
-!pip install -q xformers==0.0.20 triton==2.0.0 -U
+    
     
     #if root_path == "/content":
     #    env = "Colab"      
