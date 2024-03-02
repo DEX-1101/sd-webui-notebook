ca0695e619b2284d18b239dde91bde7b90ecdaa4
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-03-03 01:35:30 +0700

    Update x1101.py

--- res/x1101.py
+++ res/x1101.py
@@ -20,8 +20,8 @@ def kontolondon(oppai, asu, si_kontol, kntl, debug=True):
     start_time = time.time() 
     cprint(f"    > {asu}", color="flat_cyan")
     try:
-        subprocess.run(oppai, check=True, shell=True, text=True)  # for debug
-        #subprocess.run(oppai, check=True, shell=True, text=True,  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        #subprocess.run(oppai, check=True, shell=True, text=True)  # for debug
+        subprocess.run(oppai, check=True, shell=True, text=True,  stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         si_kontol += 1
     except subprocess.CalledProcessError as e:
         print(f"Error at [{asu}]: {e}")
