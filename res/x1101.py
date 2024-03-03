6b6c8ae7dbd68c5600ff365e7c9afd0d6029162b
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-03-03 11:11:07 +0700

    Update x1101.py

--- res/x1101.py
+++ res/x1101.py
@@ -54,9 +54,8 @@ if __name__ == "__main__":
     if 'content' in os.listdir('/'):
         agus.append(("pip install xformers==0.0.22.post7", "Installing xformers..."))
     elif 'kaggle' in os.listdir('/'):
-        agus.append((""))
-        #agus.append(("pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0", "Installing torch..."))
-        #agus.append(("pip install xformers==0.0.22.post7 triton==2.1.0", "Installing xformers..."))
+        agus.append(("pip install torch==2.1.2+cu121 torchvision==0.16.2+cu121 torchaudio==2.1.2 --extra-index-url https://download.pytorch.org/whl/cu121", "Installing torch..."))
+        agus.append(("pip install xformers==0.0.23.post1 triton==2.1.0", "Installing xformers..."))
     else:
         agus.append((""))
             
