d50130f9cba40750d24bba1c5a17f12da886f09f
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-05-31 15:39:11 +0700

    Update x1101.py

--- x1101.py
+++ x1101.py
@@ -32,7 +32,7 @@ def run_subprocesses_f():
         subprocess.run("pip install -q git+https://github.com/DEX-1101/colablib", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run("apt -y install -qq aria2", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run("pip install colorama", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        subprocess.run("install trash-cli && trash-put /opt/conda/lib/python3.10/site-packages/aiohttp*", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run("install trash-cli && trash-put /opt/conda/lib/python3.10/site-packages/aiohttp*", shell=True)
     progress_done = True
     
 def run_subprocesses_x():
