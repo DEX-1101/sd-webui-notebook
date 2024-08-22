22a62b55db4cc1cab1d9df2495ba0cd9b54c0294
x1101 <71503763+DEX-1101@users.noreply.github.com>
2024-08-22 13:09:34 +0700

    Update x1101.py

--- x1101.py
+++ x1101.py
@@ -45,7 +45,7 @@ def run_subprocesses_x():
         else:
             subprocess.run(f"pip install xformers=={x_ver} --no-deps", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     elif 'kaggle' in os.listdir('/') and not os.path.exists("x1101"):
-        x_ver = "0.0.26.post1"
+        x_ver = "0.0.27"
         #cprint(f"Installing xformers {x_ver}...", color="red")
         if args.debug:
             subprocess.run(f"pip install xformers=={x_ver}", shell=True)
